302a58f62fdff1402c60799a941c1fca73eae70b
gardehenry <garde.henry@uphsl.edu.ph>
2025-12-05 16:36:25 +0800

    Delete act7_Henry directory

--- act7_Henry/src/act7_Henry/whiteBoard.java
+++
@@ -1,8 +0,0 @@
-package act7_Henry;
-
-public class whiteBoard {
-    int addNumbers(int addend1, int addend2) {
-        int sum = addend1 + addend2; // compute the sum
-        return sum; // return the result
-    }
-}
