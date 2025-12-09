982b5dc9258829bf02e1d72afc2b81b6d19270a2
gardehenry <garde.henry@uphsl.edu.ph>
2025-12-09 16:09:17 +0800

    Add files via upload

---
+++ seatwork4_abstract_classes/MSWord.java
@@ -0,0 +1,19 @@
+
+public class MSWord extends Program {
+
+    public MSWord(String name) {
+        this.setName(name);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Opening MS Word...");
+        this.setIsRunning(true);
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stopping MS Word...");
+        this.setIsRunning(false);
+    }
+}
